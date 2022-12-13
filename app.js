1ecdc98946d768d476ab62542e08892400721293
Thomas Augot <thomas.augot@hotmail.fr>
2022-12-13 20:14:51 +0100

    Lab finished

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // our first Route
-app.get('/', (request, response, next) => {
+app.get("/", (request, response, next) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/gallery", (request, response, next) => {
+  response.sendFile(__dirname + "/views/gallery.html");
+});
+
+app.get("/career", (request, response, next) => {
+  response.sendFile(__dirname + "/views/career.html");
+});
+
+app.get("/about", (request, response, next) => {
+  response.sendFile(__dirname + "/views/about.html");
+});
 
 app.listen(3001, () => console.log());
